e3f31decd02866d4eba2687fb306266a6c9bc3c3
Luke <lukecochrane2@gmail.com>
2020-08-15 23:51:27 -0400

    Sending message event back out when a socket sends one to the server.

---
+++ server/src/subscribers/socket-client-messages.ts
@@ -0,0 +1,36 @@
+import type { Server, Socket } from 'socket.io';
+import { Container } from 'typedi';
+import type { Logger } from 'winston';
+import Events from './socket-event-names';
+
+export default function socketClientMessages({
+  socket,
+  room,
+}: {
+  socket: Socket,
+  room: string,
+}) {
+  const logger: Logger = Container.get('logger');
+  const io: Server = Container.get('io');
+
+  // Default chat message
+  socket.on(Events.chatMessage, (msg: string) => {
+    logger.info(msg);
+
+    // broadcast to everyone in the room
+
+    /*
+    * Think it's best to send this even to socket who
+    * wrote it so we don't have to deal with updating
+    * the state differently based on who wrote the message.
+    * If we didn't, we wouldn't have the id. However, because
+    * the id is only used for react and we're not saving it,
+    * perhaps we could just make the id on the frontend?
+    */
+    io.to(room).emit(Events.chatMessage, {
+      id: (Math.random() + 1).toString(36).substring(7),
+      msg,
+      userId: socket.id,
+    });
+  });
+}
