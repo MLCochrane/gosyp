1810c8732372f53b484a919076b459914da252b1
MLCochrane <lukecochrane2@gmail.com>
2020-12-28 16:26:43 -0500

    Update socketio.ts

--- server/src/loaders/socketio.ts
+++ server/src/loaders/socketio.ts
@@ -2,7 +2,12 @@ import type { Server } from 'http';
 import IOServer from 'socket.io';
 
 export default async ({ httpServer }: { httpServer: Server }) => {
-  const io = new IOServer(httpServer);
+  const io = new IOServer(httpServer, {
+    cors: {
+      origin: 'https://gosyp.io',
+      methods: ['GET', 'POST'],
+    },
+  });
   // can add middleware with io.use((socket, next) => {});
   // https://socket.io/docs/namespaces/
   return io;
