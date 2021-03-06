9518191eb3fdf427de2cc058bf9ad3ec9d82d0cf
MLCochrane <lukecochrane2@gmail.com>
2021-03-05 21:25:44 -0500

    Adding favicons and postbuild script (#117)

---
+++ client/postbuild.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const readDir = util.promisify(fs.readdir);
+const copyFile = util.promisify(fs.copyFile);
+
+const folderPath = './seo';
+const finalPath = './build/seo';
+
+(async function moveSeoFolder(seoFolder, dest) {
+  // Check for dir and make it if it doesn't exist
+  fs.access(dest, (err) => {
+    if (err) {
+      fs.mkdir(dest, async (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
+  try {
+    const seoFiles = await readDir(seoFolder);
+    if (seoFiles) {
+      // Wait to copy over all files to new dest
+      Promise.all(seoFiles.map(file => {
+        copyFile(path.join(__dirname, seoFolder, file), path.join(__dirname, dest, file));
+      }));
+    }
+  } catch (err) {
+    console.log(err);
+  }
+})(folderPath, finalPath);
