a7a5373b6b9e98be6d8f6b1ecb2cda3ca0e5f451
Luke <lukecochrane2@gmail.com>
2021-04-19 20:04:22 -0400

    updating client tests

--- client/src/view/components/sidebar/Sidebar.spec.tsx
+++ client/src/view/components/sidebar/Sidebar.spec.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import Sidebar from './Sidebar';
-import Details from './details/DetailWidget';
 
-it('renders childrens', () => {
-  const wrapper = shallow(<Sidebar />);
-  expect(wrapper.find(Details)).toHaveLength(2);
+it('renders without crashing', () => {
+  render(<Sidebar />);
 });
