73c7a542e6e64c5dbfb95ccdec612dc04a624504
Luke <lukecochrane2@gmail.com>
2021-04-20 19:58:03 -0400

    CreateForm tests

---
+++ client/src/view/components/homepage/create/CreateForm.spec.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import io, { Socket } from 'socket.io-client';
+import CreateForm from './CreateForm';
+
+jest.mock('socket.io-client', () => {
+  const emit = jest.fn();
+  const on = jest.fn();
+  const socket = { emit, on };
+  return jest.fn(() => socket);
+});
+const mockedIO = io as jest.Mocked<typeof io>;
+const mockedSocket = mockedIO() as jest.Mocked<typeof Socket>;
+
+it('renders form', () => {
+  render(<CreateForm />);
+  expect(screen.getByRole('form')).toBeDefined();
+  expect(screen.getByRole('textbox', { name: 'Room Name' })).toBeDefined();
+  expect(screen.getByText('Optional name for your chat room')).toBeDefined();
+  expect(screen.getByRole('textbox', { name: 'Your Nickname' })).toBeDefined();
+  expect(screen.getByText('Optional display name')).toBeDefined();
+  expect(screen.getByRole('button', { name: 'Create Room' })).toBeDefined();
+});
+
+it('emits event when submitted', async () => {
+  const mockedEmit = (mockedSocket.emit as jest.Mock).mockImplementationOnce(
+    (event, message) => message,
+  );
+
+  // Empty fields
+  const { rerender } = render(<CreateForm />);
+  const roomInput = screen.getByRole('textbox', { name: 'Room Name' });
+  const nicknameInput = screen.getByRole('textbox', { name: 'Your Nickname' });
+  const button = screen.getByRole('button', { name: 'Create Room' });
+  userEvent.click(button);
+  expect(mockedEmit).toHaveBeenCalledWith('createRoom', { name: '', nickname: '' });
+
+  // Just room name
+  rerender(<CreateForm />);
+  await userEvent.type(roomInput, 'My Room', { delay: 1 });
+  userEvent.click(button);
+  expect(mockedEmit).toHaveBeenCalledWith('createRoom', { name: 'My Room', nickname: '' });
+
+  // Both fields
+  rerender(<CreateForm />);
+  await userEvent.type(roomInput, 'Chill Zone', { delay: 1 });
+  await userEvent.type(nicknameInput, 'Phil', { delay: 1 });
+  userEvent.click(button);
+  expect(mockedEmit).toHaveBeenCalledWith('createRoom', { name: 'Chill Zone', nickname: 'Phil' });
+});
+
+it('requests room access once room successfully created', () => {
+  const mockedEmit = (mockedSocket.emit as jest.Mock).mockImplementationOnce(
+    (event, message) => message,
+  );
+  (mockedSocket.on as jest.Mock).mockImplementationOnce((event, cb) => cb({
+    message: {
+      uuid: '125f',
+      nickname: 'Rebecca',
+    },
+  }));
+  render(<CreateForm />);
+  expect(mockedEmit).toHaveBeenCalledWith('addMeToRoom', { 'room-id': '125f', nickname: 'Rebecca' });
+});
--- client/src/view/components/homepage/join/JoinForm.spec.tsx
+++ client/src/view/components/homepage/join/JoinForm.spec.tsx
@@ -13,34 +13,32 @@ jest.mock('socket.io-client', () => {
 const mockedIO = io as jest.Mocked<typeof io>;
 const mockedSocket = mockedIO() as jest.Mocked<typeof Socket>;
 
-describe('Join Form', () => {
-  it('renders form', () => {
-    render(<JoinForm />);
-    expect(screen.getByRole('form')).toBeDefined();
-    expect(screen.getByRole('textbox', { name: 'Room ID' })).toBeDefined();
-    expect(screen.getByText('Room ID that has been shared with you')).toBeDefined();
-    expect(screen.getByRole('textbox', { name: 'Nickname' })).toBeDefined();
-    expect(screen.getByText('Optional display name')).toBeDefined();
-    expect(screen.getByRole('button', { name: 'Join Room' })).toBeDefined();
-  });
+it('renders form', () => {
+  render(<JoinForm />);
+  expect(screen.getByRole('form')).toBeDefined();
+  expect(screen.getByRole('textbox', { name: 'Room ID' })).toBeDefined();
+  expect(screen.getByText('Room ID that has been shared with you')).toBeDefined();
+  expect(screen.getByRole('textbox', { name: 'Nickname' })).toBeDefined();
+  expect(screen.getByText('Optional display name')).toBeDefined();
+  expect(screen.getByRole('button', { name: 'Join Room' })).toBeDefined();
+});
 
-  it('prepopulates form with room from query', () => {
-    Object.defineProperty(window, 'location', { writable: true, configurable: true, value: { search: '?roomId=gh185' } });
-    render(<JoinForm />);
-    expect((screen.getByRole('textbox', { name: 'Room ID' }) as HTMLInputElement).value).toBe('gh185');
-  });
+it('prepopulates form with room from query', () => {
+  Object.defineProperty(window, 'location', { writable: true, configurable: true, value: { search: '?roomId=gh185' } });
+  render(<JoinForm />);
+  expect((screen.getByRole('textbox', { name: 'Room ID' }) as HTMLInputElement).value).toBe('gh185');
+});
 
-  it('emits event on submit', async () => {
-    const mockedEmit = (mockedSocket.emit as jest.Mock).mockImplementationOnce(
-      (event, message) => message,
-    );
-    render(<JoinForm />);
-    userEvent.clear(screen.getByRole('textbox', { name: 'Room ID' }));
-    // Have to add waitFor as our form sets state asynchronously and no delay option
-    await waitFor(() => expect((screen.getByRole('textbox', { name: 'Room ID' }) as HTMLInputElement).value).toBe(''));
-    await userEvent.type(screen.getByRole('textbox', { name: 'Room ID' }), 'bhja1hf', { delay: 1 });
-    await userEvent.type(screen.getByRole('textbox', { name: 'Nickname' }), 'Frank', { delay: 1 });
-    userEvent.click(screen.getByRole('button', { name: 'Join Room' }));
-    expect(mockedEmit).toHaveBeenCalledWith('addMeToRoom', { 'room-id': 'bhja1hf', nickname: 'Frank' });
-  });
+it('emits event on submit', async () => {
+  const mockedEmit = (mockedSocket.emit as jest.Mock).mockImplementationOnce(
+    (event, message) => message,
+  );
+  render(<JoinForm />);
+  userEvent.clear(screen.getByRole('textbox', { name: 'Room ID' }));
+  // Have to add waitFor as our form sets state asynchronously and no delay option
+  await waitFor(() => expect((screen.getByRole('textbox', { name: 'Room ID' }) as HTMLInputElement).value).toBe(''));
+  await userEvent.type(screen.getByRole('textbox', { name: 'Room ID' }), 'bhja1hf', { delay: 1 });
+  await userEvent.type(screen.getByRole('textbox', { name: 'Nickname' }), 'Frank', { delay: 1 });
+  userEvent.click(screen.getByRole('button', { name: 'Join Room' }));
+  expect(mockedEmit).toHaveBeenCalledWith('addMeToRoom', { 'room-id': 'bhja1hf', nickname: 'Frank' });
 });
