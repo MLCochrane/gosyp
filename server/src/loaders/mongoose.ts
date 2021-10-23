0c7ad712703b049d6e7e5afe06ceec4ec359804a
Luke <lukecochrane2@gmail.com>
2021-10-22 22:38:38 -0400

    WIP: updating server init in docker

--- server/src/loaders/mongoose.ts
+++ server/src/loaders/mongoose.ts
@@ -2,6 +2,15 @@ import mongoose from 'mongoose';
 import { Db } from 'mongodb';
 import config from '../config';
 
+/**
+ * TODO:
+ * - Add error handler for failing to connect on first connection
+ * - in error handler, attempt the reconnection a few times before finally
+ *   considering the DB inaccessable.
+ *
+ * This should help for initializing docker when DB is not yet up but containers
+ * have finished building.
+ */
 export default async (): Promise<Db> => {
   const connection = await mongoose.connect(
     config.databaseURL as string,
